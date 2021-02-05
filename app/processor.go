a696914f0ce885cd992fb98b19af834fc0a2e647
Joshua Raphael Fuentes <joshuarpf@gmail.com>
2021-02-05 22:51:56 +0800

    Created a processor method to generate data for the summaries

--- app/processor.go
+++ app/processor.go
@@ -1,6 +1,32 @@
 package app
 
-// Processor takes care of the process execution
-type Processor struct {
-	ProcessedIDs []int
+import (
+	"fmt"
+	"helper"
+	"model"
+	"payload"
+)
+
+// getSummaries returns the summaries of scores based on groups
+func getSummaries(inputScores payload.InputScores) string {
+	inputMap := helper.GetInputMap(inputScores)
+
+	for key, value := range inputMap {
+
+		if key == "managers" {
+			groupScore := model.ManagersScore{}
+			groupScore.Name = key
+			groupScore.ScoreCollection = value
+			fmt.Println(groupScore)
+			fmt.Println(groupScore.GetAverageScore())
+		} else {
+			groupScore := model.NonManagersScore{}
+			groupScore.Name = key
+			groupScore.ScoreCollection = value
+			fmt.Println(groupScore)
+			fmt.Println(groupScore.GetAverageScore())
+		}
+	}
+
+	return "END OF PROCES"
 }
