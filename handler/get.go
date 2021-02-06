abb903839f31b9b3cd4b0321268e284cb8fb6536
Joshua Raphael Fuentes <joshuarpf@gmail.com>
2021-02-06 20:23:41 +0800

    minor cleanup

--- handler/get.go
+++ handler/get.go
@@ -1,22 +1,12 @@
 package handler
 
 import (
-	"encoding/json"
+	"fmt"
 	"net/http"
-	"payload"
+	"os"
 )
 
 // Test is a test function
 func Test(writer http.ResponseWriter, request *http.Request) {
-
-	sampleErrors := []string{"one", "tow", "another"}
-
-	test := payload.Output{
-		Success: false,
-		Errors:  sampleErrors,
-	}
-
-	writer.Header().Set("Content-Type", "application/json; charset=utf-8")
-
-	json.NewEncoder(writer).Encode(test)
+	fmt.Println(os.Getenv("MAX_SCORE"))
 }
