4a713a34053a87e3b6188015a7adccc2c3b1706c
Joshua Raphael Fuentes <joshuarpf@gmail.com>
2021-02-19 10:57:13 +0800

    Refactored to return values and omit the need to pass the "writer" into this

--- app/controller.go
+++ app/controller.go
@@ -1,17 +1,15 @@
 /**
- * This provides the controller layer of the app. It is responsibile for beign 
- * the middle man between the model and the one that generates the output. 
- * 
- * I strongly believe that controllers should only be middlemans, which means it should 
- * not be doing all the processing, but rather calling other resources to do it. 
- * 
+ * This provides the controller layer of the app. It is responsibile for beign
+ * the middle man between the model and the one that generates the output.
+ *
+ * I strongly believe that controllers should only be middlemans, which means it should
+ * not be doing all the processing, but rather calling other resources to do it.
+ *
  * I normally kepe my controller methods light.
  */
 package app
 
 import (
-	"helper"
-	"net/http"
 	"payload"
 )
 
@@ -19,10 +17,16 @@ import (
 type Controller struct{}
 
 // Compute fetches computation of averages
-func (controller *Controller) Compute(inputScores payload.InputScores, writer http.ResponseWriter) {
+func (controller *Controller) Compute(inputScores payload.InputScores) (summary map[string]interface{}, error interface{}) {
 
 	summaryOfAverages := getSummaries(inputScores)
 
+	if summaryOfAverages != nil {
+		return summaryOfAverages, nil
+	} else {
+		return nil, "No data ..."
+	}
+
 	// Triggers to send the output out after processing
-	helper.DisplayOutput(true, summaryOfAverages, []string{}, writer)
+	// helper.DisplayOutput(true, summaryOfAverages, []string{}, writer)
 }
